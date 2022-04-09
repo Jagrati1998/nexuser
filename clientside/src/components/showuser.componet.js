e2337a36af25acb16175a019734320a362f4cb13
Jagrati Mishra <jagratimishra2222@gmail.com>
2022-04-09 22:15:33 +0530

    show user and user list commit

---
+++ clientside/src/components/showuser.componet.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import axios from 'axios';
+import Table from 'react-bootstrap/Table';
+import Userlist from './userlistComponent';
+
+export default class ShowUser extends Component {
+  state = 
+   {
+     users: []
+   }
+
+   componentDidMount()
+    {
+    axios.get('http://localhost:4000/users/')
+    .then(res => { this.setState({users: res.data });
+     })
+     .catch((error) => {
+      console.log(error);
+    })
+    }
+    DataTable()
+    {
+      return this.state.users.map((res, i) => {
+      return <Userlist obj={res} key={i} />;
+    });
+  }
+ render() {
+      return (
+      <div className="table-wrapper-showuser">
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Date Of Birth</th>
+              <th>Email</th>
+              <th>Action</th>
+              </tr>
+          </thead>
+          <tbody>
+          {this.DataTable()}
+          </tbody>
+      </Table>
+      </div>
+    );
+  }
+}
+  
---
+++ clientside/src/components/userlist.componet.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import Icon from '@mui/material/Icon';
+import BorderColorIcon from '@mui/icons-material/BorderColor';
+import DeleteIcon from '@mui/icons-material/Delete';
+//import Avatar from '@mui/material/Avatar';
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import Button from 'react-bootstrap/Button'
+const moment = require('moment');
+moment().format('YYYY-MM-DD');
+export default class Userlist extends Component 
+{
+  constructor(props) 
+  {
+    super(props)
+    this.deleteUser = this.deleteUser.bind(this)
+  }
+
+  deleteUser() {
+    axios
+      .delete(
+        'http://localhost:4000/users/delete-user/' + this.props.obj._id,
+      )
+      .then((res) => {
+        console.log('user successfully deleted!')
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    }
+ 
+  render()
+ { 
+   return 
+   (
+     
+    <tr>
+    <td>{this.props.obj.imagepath}</td>
+    <td>{this.props.obj.fname}</td>
+    <td>{this.props.obj.lname}</td>
+    <td>{moment(this.props.obj.dob).format('YYYY-MM-DD')}</td>
+    <td>{this.props.obj.email}</td>
+    <td>
+    <Link
+         className="edit-link" path={"/show-user/"}
+         to={'/update-user/' + this.props.obj._id}
+         >
+        <BorderColorIcon/>
+    </Link>
+    <Button onClick={this.deleteUser} size="sm" variant="danger"> <DeleteIcon/> </Button> 
+    </td>
+    </tr>
+    )
+    }
+}
+        
+    
+   
+   
+   
+
+    
+       
+       
+       
+      
+      
+       
+     
+   
